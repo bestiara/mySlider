1370bee5991d1e9f4f8adb7aebe9cb022ec122b1
bestiara <dmitry.nickolskiy@mail.ru>
2014-10-07 12:32:54 +0400

    07-10-2014

--- js/mySlider-js.js
+++ js/mySlider-js.js
@@ -53,11 +53,10 @@
             startAutoSlide();
 
             function setSlider() { //создание слайдера, согласно конфигам
-                //todo организовать создание управляющих элементов, в зависимости от количества и содержимого слайдов
-                if (settings.arrows == true) {
+                if (settings.arrows) { //добавление стрелок
                     $this.append('<div class="arrow prev"></div><div class="arrow next"></div>');
                 }
-                if (settings.selector == true) {
+                if (settings.selector) { //создание шариков-селекторов
                     $controlSlide.addClass('active');
                     $slide.each(function () {
                         $clone = $controlSlide.clone();
@@ -70,104 +69,162 @@
                     });
                     $this.append($sliderControls);
                 }
-                if (settings.thumbnail == false) {
+                if (!settings.thumbnail) {
+
                     $this.height(slideHeight);
                     $this.width(slideWidth);
+
                     $slide.height(slideHeight);
                     $slide.width(slideWidth);
+
                 }
-                else if (settings.thumbnail == true) {
+
+                else {
+
                     switch (settings.thumbPos) {
+
                         case 'bottom':
+
                             $this.height(slideHeight - slideHeight / slideCount);
                             $this.width(slideWidth);
+
                             $slide.height(slideHeight - slideHeight / slideCount);
                             $slide.width(slideWidth);
+
                             $thumb.width($slide.width() / slideCount - 5);
                             $thumb.height($slide.height() / slideCount);
+
                             break;
+
                         case 'left':
+
                             slideWidth = slideWidth - slideWidth / slideCount;
+
                             $this.width(slideWidth);
+
                             $slide.width(slideWidth);
                             $slide.height(slideHeight);
+
                             $thumb.width($slide.width() / slideCount);
                             $thumb.height($slide.height() / slideCount - 5);
+
                             $this.css("margin-left", $this.width() / slideCount + 5 + 'px');
                             $this.css('float', 'left');
+
                             $thumb.css('margin', '0 5px  5px  0 ');
                             $thumb.css('float', 'none');
+
                             break;
+
                         case 'top':
+
                             $this.height(slideHeight - slideHeight / slideCount);
                             $this.width(slideWidth);
+
                             $slide.height(slideHeight - slideHeight / slideCount);
                             $slide.width(slideWidth);
+
                             $thumb.width($slide.width() / slideCount - 5);
                             $thumb.height($slide.height() / slideCount);
                             $thumb.css('margin', '0 5px 5px 0');
+
                             $this.css("margin-top", $slide.height() / slideCount + 5 + 'px');
+
                             $thumbnail.css('position', 'static');
+
                             break;
+
                         case 'right':
+
                             if (settings.links == true) {
+
                                 slideWidth = slideWidth - slideWidth / (slideCount + 2);
+
                                 $this.width(slideWidth);
+
                                 $slide.width(slideWidth);
                                 $slide.height(slideHeight);
+
                                 $thumb.width($slide.width() / slideCount);
                                 $thumb.height($slide.height() / slideCount - 5);
+
                                 $thumbnail.css('margin-left', slideWidth);
+
                                 $thumb.css('margin', '0 0 5px 5px');
                                 $thumb.css('float', 'none');
+
                                 $this.css('float', 'left');
                                 $this.width(slideWidth);
+
                             }
                             slideWidth = slideWidth - slideWidth / slideCount;
+
                             $this.width(slideWidth);
+
                             $slide.width(slideWidth);
                             $slide.height(slideHeight);
+
                             $thumb.width($slide.width() / slideCount);
                             $thumb.height($slide.height() / slideCount - 5);
+
                             $thumbnail.css('margin-left', slideWidth);
+
                             $thumb.css('margin', '0 0 5px 5px');
                             $thumb.css('float', 'none');
+
                             $this.css('float', 'left');
                             $this.width(slideWidth);
+
                             break;
+
                     }
                 }
+                //настройка миниатюр
                 var image = new Image();
+
                 $thumb.addClass('active');
+
                 $slide.each(function () {
+
                     var $slide = $(this),
                         $img = $slide.find('img');
                     image.src = $img.attr('src');
                     if (settings.links == true) {
+
                         $link.attr('href', $slide.data('url'));
                         $link.css({
                             'margin-left': $thumb.width() + 10,
                             'padding-top': $thumb.height() / 2 - 10
                         });
                         $link.html($slide.data('text'));
+
                         $thumb.append($link);
+
                     }
+
                     $thumb.css('background-image', 'url(' + image.src + ')');
+
                     $clone = $thumb.clone();
                     $clone.click(function (event) {
                         event.preventDefault();
                         setSlide($(this));
                     });
+
                     $thumbContainer.append($clone);
                     $thumb.removeClass('active');
+
                     if ($slide.width() / image.width > $slide.height() / image.height) {
                         $img.width($slide.width());
                     } else {
                         $img.height($slide.height());
                     }
+
                     $thumbnail.append($thumbContainer);
+
                     $this.parent().append($thumbnail);
+
                 });
+
             }
 
             $this.hover(function () { //стопаем автоскролл при наведении на слайдер
